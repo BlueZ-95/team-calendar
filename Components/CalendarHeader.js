92d57bcfd808e0e62afd7725c07a0a5fcaf4290d
BlueZ <62077144+BlueZ-95@users.noreply.github.com>
2021-05-11 23:35:01 +0530

    sync

--- Components/CalendarHeader.js
+++ Components/CalendarHeader.js
@@ -9,8 +9,8 @@ function CalendarHeader() {
 
     return (
         <div className="w-full h-16 flex items-center justify-between px-10">
-            <h3 className="text-2xl font-medium">May 2021 <span className="text-gray-300">{'<>'}</span></h3>
-            {/* <p className="text-2xl font-medium">Week / Month</p> */}
+            <h3 className="text-2xl font-medium">May 2021 <span className="text-gray-300 text-xl font-semibold">{'<>'}</span></h3>
+
             <div className="flex items-center justify-center w-48 h-2/3 bg-blue-100 rounded-lg px-1 text-sm text-center font-medium">
                 <span className={`block w-1/2 mx-1 py-1 cursor-pointer ${isWeekSelected ? 'rounded-md shadow-lg text-blue-500 bg-white' : 'text-gray-400'}`} onClick={e => selectWeekOrMonth(e, true)}>Week</span>
                 <span className={`block w-1/2 mx-1 py-1 cursor-pointer ${!isWeekSelected ? 'rounded-md shadow-lg text-blue-500 bg-white' : 'text-gray-400'}`} onClick={e => selectWeekOrMonth(e, false)}>Month</span>
