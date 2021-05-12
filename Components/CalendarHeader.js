7e03a62ec1f60f89a218486544e9888bc6152714
BlueZ <62077144+BlueZ-95@users.noreply.github.com>
2021-05-12 23:06:46 +0530

    add new note ui updated

--- Components/CalendarHeader.js
+++ Components/CalendarHeader.js
@@ -18,8 +18,16 @@ function CalendarHeader() {
                 </div>
                 <span className="w-10 h-10 bg-green-500 ml-5 rounded-lg cursor-pointer hover:shadow-md"><p className="text-3xl font-light flex items-center justify-center text-white">+</p></span>
 
-                <div className="absolute w-64 h-72 bg-green-200 top-16 rounded-xl">
-                    
+                <div className="absolute w-64 h-96 bg-green-300 top-16 rounded-xl p-5">
+                    <form>
+                        <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="author" placeholder="Author" type="text" />
+                        <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="cardContent" placeholder="Content" type="text" />
+                        <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="date" placeholder="Date" type="text" />
+                        <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="day" placeholder="Day" type="text" />
+                        <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="hour" placeholder="Hour" type="text" />
+                        <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="month" placeholder="Month" type="text" />
+                        <button type="button" className="w-full h-full text-white p-2 my-3 rounded-md bg-green-500 ring-offset-4 ring-green-500 ring-offset-green-300 focus:ring-2">Add New Note</button>
+                    </form>
                 </div>
             </span>
 
