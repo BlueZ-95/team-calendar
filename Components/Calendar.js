5cc781053ec7df89040d8c15b863c722dc0e10a2
Saad <saad.patel@thinksense.ai>
2021-05-12 13:24:59 +0530

    added week calendar

--- Components/Calendar.js
+++ Components/Calendar.js
@@ -1,9 +1,11 @@
 import CalendarHeader from "./CalendarHeader";
+import WeekCalendar from "./WeekCalendar";
 
 function Calendar() {
     return (
-        <div className="w-full h-full flex flex-col items-center justify-between">
+        <div className="w-full h-full flex flex-col items-center justify-self-start">
             <CalendarHeader />
+            <WeekCalendar />
         </div>
     )
 }
