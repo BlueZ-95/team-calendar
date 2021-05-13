5373175442d8ec9bd8660163c6b0439d5aa608ba
Saad <saad.patel@thinksense.ai>
2021-05-13 13:44:24 +0530

    update

--- Components/CalendarHeader.js
+++ Components/CalendarHeader.js
@@ -25,7 +25,7 @@ function CalendarHeader({ addNote }) {
 
                 <span className="w-10 h-10 bg-blue-500 ml-5 rounded-lg cursor-pointer select-none hover:shadow-md"><p className="text-3xl font-light flex items-center justify-center text-white" onClick={() => toggleAddNewNote()}>+</p></span>
 
-                <div className={`transform ${isFormVisible ? 'scale-y-100' : 'scale-y-0'} origin-top transition duration-75 ease-out absolute w-64 h-96 bg-blue-300 top-16 rounded-lg p-5 select-none`}>
+                <div className={`transform ${isFormVisible ? 'scale-100' : 'scale-0'} origin-top-right transition duration-75 ease-out absolute w-64 h-96 bg-blue-300 top-16 rounded-lg p-5 select-none`}>
                     <form onSubmit={addNote}>
                         <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="author" placeholder="Author" type="text" />
                         <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="cardContent" placeholder="Content" type="text" />
