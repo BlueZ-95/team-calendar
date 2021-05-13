d600dd93a66c7043e4b5d10f015f1c04e683d23d
Saad <saad.patel@thinksense.ai>
2021-05-13 13:29:15 +0530

    added week dates

--- Components/CalendarHeader.js
+++ Components/CalendarHeader.js
@@ -19,13 +19,13 @@ function CalendarHeader({ addNote }) {
 
             <span className="relative h-full flex items-center justify-between">
                 <div className="flex items-center justify-center w-48 h-2/3 bg-blue-100 rounded-lg px-1 text-sm text-center font-medium">
-                    <span className={`block w-1/2 mx-1 py-1 cursor-pointer ${isWeekSelected ? 'rounded-md shadow-lg text-blue-500 bg-white' : 'text-gray-400'}`} onClick={e => selectWeekOrMonth(e, true)}>Week</span>
-                    <span className={`block w-1/2 mx-1 py-1 cursor-pointer ${!isWeekSelected ? 'rounded-md shadow-lg text-blue-500 bg-white' : 'text-gray-400'}`} onClick={e => selectWeekOrMonth(e, false)}>Month</span>
+                    <span className={`block w-1/2 mx-1 py-1 cursor-pointer transition duration-200 ease-out ${isWeekSelected ? 'rounded-md shadow-lg text-blue-500 bg-white' : 'text-gray-400'}`} onClick={e => selectWeekOrMonth(e, true)}>Week</span>
+                    <span className={`block w-1/2 mx-1 py-1 cursor-pointer transition duration-200 ease-out ${!isWeekSelected ? 'rounded-md shadow-lg text-blue-500 bg-white' : 'text-gray-400'}`} onClick={e => selectWeekOrMonth(e, false)}>Month</span>
                 </div>
 
                 <span className="w-10 h-10 bg-blue-500 ml-5 rounded-lg cursor-pointer select-none hover:shadow-md"><p className="text-3xl font-light flex items-center justify-center text-white" onClick={() => toggleAddNewNote()}>+</p></span>
 
-                <div className={`transform ${isFormVisible ? 'scale-y-100' : 'scale-y-0'} origin-top transition duration-150 ease-out absolute w-64 h-96 bg-blue-300 top-16 rounded-xl p-5 select-none`}>
+                <div className={`transform ${isFormVisible ? 'scale-y-100' : 'scale-y-0'} origin-top transition duration-75 ease-out absolute w-64 h-96 bg-blue-300 top-16 rounded-xl p-5 select-none`}>
                     <form onSubmit={addNote}>
                         <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="author" placeholder="Author" type="text" />
                         <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="cardContent" placeholder="Content" type="text" />
--- Components/WeeklyCards.js
+++ Components/WeeklyCards.js
@@ -2,10 +2,23 @@
 import Card from "./Card"
 
 function WeeklyCards({ notes }) {
+
+    //Get current week dates
+    let currDate = new Date();
+    let currentWeek = [];
+
+    for (let i = 1; i < 7; i++) {
+        let first = currDate.getDate() - currDate.getDay() + i;
+        let day = new Date(currDate.setDate(first));
+        currentWeek.push(day)
+    }
+    //
+
+
     return (
         <div className="w-full flex mt-5 overflow-hidden">
             <div className="w-1/6 h-full flex flex-col items-center justify-start">
-                <h1 className="font-medium text-2xl text-gray-400">Monday</h1>
+                <h1 className="font-medium text-2xl text-gray-400">Monday <p className="text-center text-base">{currentWeek[0].getDate()}</p></h1>
                 {
                     notes.hasOwnProperty('Monday') && notes.Monday.map(note => {
                         return <Card cardContent={note.cardContent} time={note.hour} />
@@ -13,7 +26,7 @@ function WeeklyCards({ notes }) {
                 }
             </div>
             <div className="w-1/6 h-full flex flex-col items-center justify-start">
-                <h1  className="font-medium text-2xl text-gray-400">Tuesday</h1>
+                <h1  className="font-medium text-2xl text-gray-400">Tuesday <p className="text-center text-base">{currentWeek[1].getDate()}</p></h1>
                 {
                     notes.hasOwnProperty('Tuesday') && notes.Tuesday.map(note => {
                         return <Card cardContent={note.cardContent} time={note.hour} />
@@ -21,7 +34,7 @@ function WeeklyCards({ notes }) {
                 }
             </div>
             <div className="w-1/6 h-full flex flex-col items-center justify-start">
-                <h1  className="font-medium text-2xl text-gray-400">Wednesday</h1>
+                <h1  className="font-medium text-2xl text-gray-400">Wednesday <p className="text-center text-base">{currentWeek[2].getDate()}</p></h1>
                 {
                     notes.hasOwnProperty('Wednesday') && notes.Wednesday.map(note => {
                         return <Card cardContent={note.cardContent} time={note.hour} />
@@ -29,7 +42,7 @@ function WeeklyCards({ notes }) {
                 }
             </div>
             <div className="w-1/6 h-full flex flex-col items-center justify-start">
-                <h1 className="font-medium text-2xl text-gray-400">Thursday</h1>
+                <h1 className="font-medium text-2xl text-gray-400">Thursday <p className="text-center text-base">{currentWeek[3].getDate()}</p></h1>
                 {
                     notes.hasOwnProperty('Thursday') && notes.Thursday.map(note => {
                         return <Card cardContent={note.cardContent} time={note.hour} />
@@ -37,7 +50,7 @@ function WeeklyCards({ notes }) {
                 }
             </div>
             <div className="w-1/6 h-full flex flex-col items-center justify-start">
-                <h1 className="font-medium text-2xl text-gray-400">Friday</h1>
+                <h1 className="font-medium text-2xl text-gray-400">Friday <p className="text-center text-base">{currentWeek[4].getDate()}</p></h1>
                 {
                     notes.hasOwnProperty('Friday') && notes.Friday.map(note => {
                         return <Card cardContent={note.cardContent} time={note.hour} />
@@ -45,7 +58,7 @@ function WeeklyCards({ notes }) {
                 }
             </div>
             <div className="w-1/6 h-full flex flex-col items-center justify-start">
-                <h1  className="font-medium text-2xl text-gray-400">Saturday</h1>
+                <h1  className="font-medium text-2xl text-gray-400">Saturday <p className="text-center text-base">{currentWeek[5].getDate()}</p></h1>
                 {
                     notes.hasOwnProperty('Saturday') && notes.Saturday.map(note => {
                         return <Card cardContent={note.cardContent} time={note.hour} />
