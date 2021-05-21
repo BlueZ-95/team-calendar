a873d67e2eb8780ae6e3f5c523efa5cf14d59ca4
BlueZ-95 <patelsaad39@gmail.com>
2021-05-21 20:52:13 +0530

    sync

--- components/CalendarHeader.js
+++ components/CalendarHeader.js
@@ -4,6 +4,9 @@ function CalendarHeader({ addNote, days }) {
     const [isWeekSelected, setIsWeekSelected] = useState(true);
     const [isFormVisible, setIsFormVisible] = useState(false);
 
+    let today = new Date();
+    let todayDate = String(today.getDate()).padStart(2, '0');
+
     const selectWeekOrMonth = (e, isWeekSelected) => {
         setIsWeekSelected(isWeekSelected);
     }
@@ -31,21 +34,24 @@ function CalendarHeader({ addNote, days }) {
 
                 <span className="w-10 h-10 bg-indigo-500 ml-5 rounded-lg cursor-pointer select-none hover:shadow-md"><p className="text-3xl font-light flex items-center justify-center text-white" onClick={() => toggleAddNewNote()}>+</p></span>
 
-                <div className={`transform ${isFormVisible ? 'scale-100' : 'scale-0'} origin-top-right transition duration-75 ease-out absolute w-64 h-96 bg-gray-200 top-16 rounded-lg p-5 select-none z-10 shadow-2xl`}>
+                <div className={`transform ${isFormVisible ? 'scale-100' : 'scale-0'} origin-top-right transition duration-75 ease-out absolute w-64  bg-gray-200 top-16 rounded-lg p-5 select-none z-10 shadow-2xl`}>
                     <form onSubmit={e => handleSubmit(e)}>
-                        <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="author" placeholder="Author" type="text" />
-                        <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="cardContent" placeholder="Content" type="text" />
-                        <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="date" placeholder="Date" type="text" />
-                        {/* <select className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="date">
+                        {/* <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="author" placeholder="Author" type="text" /> */}
+                        <p>Select Date :</p>
+                        <select className={"w-full h-8 p-2 my-2 rounded-md outline-none bg-white"} name="date">
                             {
-                                days.map(date => {
-                                    return <option key={date} value={date} text={date}></option>
+                                days.map((date, index) => {
+                                    date = date.getDate();
+                                    return (date >= todayDate && <option key={index} value={date}>{date}</option>)
                                 })
                             }
-                        </select> */}
-                        <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="day" placeholder="Day" type="text" />
-                        <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="hour" placeholder="Hour" type="text" />
-                        <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="month" placeholder="Month" type="text" />
+                        </select>
+                        <p>Content :</p>
+                        <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="cardContent" placeholder="Content" type="text" />
+                        {/* <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="date" placeholder="Date" type="text" /> */}
+                        {/* <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="day" placeholder="Day" type="text" /> */}
+                        {/* <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="hour" placeholder="Hour" type="text" />
+                        <input className={"w-full h-8 p-2 my-2 rounded-md outline-none"} name="month" placeholder="Month" type="text" /> */}
                         <button type="submit" className="w-full h-full text-white p-2 my-3 rounded-md bg-indigo-500 ring-offset-4 ring-indigo-500 ring-offset-indigo-300 focus:ring-2">Add New Note</button>
                     </form>
                 </div>
